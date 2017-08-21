8bae467426b915998ad00875ced189bba3e1d53f
Piotr Filek <dearn@dearn.pl>
2017-08-21 21:12:26 +0200

    zmiany z roboty

--- konwerter.py
+++ konwerter.py
@@ -8,16 +8,17 @@ c2 = list(csv.DictReader(f2, delimiter=';'))
 def roznicaStr(a, b):
     return int(b) - int(a)
 
-for i in range(3):
+for i in range(500):
     #        print("Nie znaleziono ",c1[i-1]['ID'])
-    for j in range(3):
+    for j in range(500):
         if( c1[i]['ID'] == c2[j]['ID']):
             print(c1[i]['ID'],c2[j]['ID'],c1[i]['Nazwa'],c1[i]['Ilosc'], c2[j]['Ilosc'], roznicaStr(c1[i]['Ilosc'],c2[j]['Ilosc']))
             znaleziono = 1
             break
         else:
             znaleziono = 0
-    if znaleziono == 0:
-        print("Nie znaleziono ",c1[i]['ID'])
+    # if znaleziono == 0:
+        # dziala ale wyciszyc
+        # print("Nie znaleziono ",c1[i]['ID'])
         
             
