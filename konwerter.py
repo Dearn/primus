08fa7cce18560dd0899cd20e405bde1c1274592c
Piotr Filek <dearn@dearn.pl>
2017-08-22 20:05:12 +0200

    Dzialajacy zapis jest real nie ma fakt.

--- konwerter.py
+++ konwerter.py
@@ -6,25 +6,35 @@ f2 = open('fakt.csv', 'r')
 c2 = list(csv.DictReader(f2, delimiter=';'))
 
 f3 = open('wynik.csv', 'w')
-filewriter = csv.writer(f3, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+filewriter = csv.writer(f3, delimiter=';', quotechar='"',
+                        quoting=csv.QUOTE_MINIMAL)
 
-filewriter.writerow(['ID','Nazwa','Ilosc Real', 'Ilosc Fakt'])
+filewriter.writerow(['ID', 'Nazwa', 'Ilosc Real', 'Ilosc Fakt'])
+lista = []
+
+
 def roznicaStr(a, b):
     return ""
 
 
-for i in range(len(c1)-1):
+for i in range(len(c1) - 1):
     #        print("Nie znaleziono ",c1[i-1]['ID'])
-    for j in range(len(c2)-1):
-        if( c1[i]['ID'] == c2[j]['ID'] and c1[i]['ID'] != "00000"):
-            # print(c1[i]['ID'],c2[j]['ID'],c1[i]['Nazwa'][:25],"\t",c1[i]['Ilosc'], c2[j]['Ilosc'], roznicaStr(c1[i]['Ilosc'],c2[j]['Ilosc']))
-            filewriter.writerow([c1[i]['ID'],c1[i]['Nazwa'],c1[i]['Ilosc'],c2[j]['Ilosc']])
+    for j in range(len(c2) - 1):
+        if(c1[i]['ID'] == c2[j]['ID'] and c1[i]['ID'] != "00000"):
+            # print(c1[i]['ID'], c2[j]['ID'], c1[i]['Nazwa'][:8], "\t", c1[i]
+            #       ['Ilosc'], c2[j]['Ilosc'], roznicaStr(c1[i]['Ilosc'], c2[j]['Ilosc']))
+            filewriter.writerow(
+                [c1[i]['ID'], c1[i]['Nazwa'], c1[i]['Ilosc'], c2[j]['Ilosc']])
             znaleziono = 1
             break
         else:
             znaleziono = 0
             # print(c1[i]['ID'],c2[j]['ID'],c1[i]['Nazwa'][:25],"\t",c1[i]['Ilosc'], c2[j]['Ilosc'], roznicaStr(c1[i]['Ilosc'],c2[j]['Ilosc']))
-            # if znaleziono == 0:
-            # dziala ale wyciszyc
-            # print("Nie znaleziono ",c1[i]['ID'])
+    if(znaleziono == 0 and c1[i]['ID'] != "00000"):
+        # dziala ale wyciszyc
+        # print("Nie znaleziono ", c1[i]['ID'])
+        lista.append([c1[i]['ID'], c1[i]['Nazwa'],
+                      c1[i]['Ilosc'],])
+for row in lista:
+    filewriter.writerow(row)
 f3.close()
