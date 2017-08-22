68fe960736b05a20dc02cd0576a2b078eb96792c
Piotr Filek <dearn@dearn.pl>
2017-08-22 19:24:15 +0200

    poprawnie generowany wynik

--- konwerter.py
+++ konwerter.py
@@ -5,20 +5,26 @@ c1 = list(csv.DictReader(f1, delimiter=';'))
 f2 = open('fakt.csv', 'r')
 c2 = list(csv.DictReader(f2, delimiter=';'))
 
-def roznicaStr(a, b):
-    return int(b) - int(a)
+f3 = open('wynik.csv', 'w')
+filewriter = csv.writer(f3, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-for i in range(500):
+filewriter.writerow(['ID','Nazwa','Ilosc Real', 'Ilosc Fakt'])
+def roznicaStr(a, b):
+    return ""
+
+
+for i in range(len(c1)-1):
     #        print("Nie znaleziono ",c1[i-1]['ID'])
-    for j in range(500):
-        if( c1[i]['ID'] == c2[j]['ID']):
-            print(c1[i]['ID'],c2[j]['ID'],c1[i]['Nazwa'],c1[i]['Ilosc'], c2[j]['Ilosc'], roznicaStr(c1[i]['Ilosc'],c2[j]['Ilosc']))
+    for j in range(len(c2)-1):
+        if( c1[i]['ID'] == c2[j]['ID'] and c1[i]['ID'] != "00000"):
+            # print(c1[i]['ID'],c2[j]['ID'],c1[i]['Nazwa'][:25],"\t",c1[i]['Ilosc'], c2[j]['Ilosc'], roznicaStr(c1[i]['Ilosc'],c2[j]['Ilosc']))
+            filewriter.writerow([c1[i]['ID'],c1[i]['Nazwa'],c1[i]['Ilosc'],c2[j]['Ilosc']])
             znaleziono = 1
             break
         else:
             znaleziono = 0
-    # if znaleziono == 0:
-        # dziala ale wyciszyc
-        # print("Nie znaleziono ",c1[i]['ID'])
-        
-            
+            # print(c1[i]['ID'],c2[j]['ID'],c1[i]['Nazwa'][:25],"\t",c1[i]['Ilosc'], c2[j]['Ilosc'], roznicaStr(c1[i]['Ilosc'],c2[j]['Ilosc']))
+            # if znaleziono == 0:
+            # dziala ale wyciszyc
+            # print("Nie znaleziono ",c1[i]['ID'])
+f3.close()
